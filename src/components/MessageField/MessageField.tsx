8d9cb28be3548357ea3480ec1ffaacafb53c3fb2
Yury Glushko <yury.glushko.web@gmail.com>
2022-08-20 21:58:49 +0300

    feat: added MessageField

--- src/components/MessageField/MessageField.tsx
+++ src/components/MessageField/MessageField.tsx
@@ -1,10 +1,46 @@
-import React from "react";
+import React, {useEffect, useMemo, useRef} from "react";
 import css from './MessageField.module.scss'
+import {useAppSelector} from "../../hooks/redux";
+import Message from "../Message/Message";
 
-export default React.memo(
-    () => {
+export default React.memo(() => {
+        /**
+         * Init data
+         */
+        const {messages} = useAppSelector(state => state.chatReducer)
+        const divEl = useRef<HTMLDivElement>(null)
+
+        /**
+         * Lifecycle
+         */
+        useEffect(
+            () => {
+                const isDivElement = divEl && divEl.current
+                if (!isDivElement) return
+
+                divEl.current.scrollTop = divEl.current.scrollHeight - divEl.current.clientHeight
+            },
+            [messages]
+        )
+
+        /**
+         * Render
+         */
+        const elements = useMemo(
+            () => {
+                return messages.map(({message, author}, idx) => {
+                    return <Message key={idx} message={message} author={author}/>
+                })
+            },
+            [messages]
+        )
+
         return (
-            <div className={css['message-field']}>
+            <div
+                className={css['message-field']}
+                ref={divEl}
+            >
+                {elements}
             </div>
         )
     }
