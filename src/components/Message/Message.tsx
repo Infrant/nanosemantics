a76a3914616c89c510d272c9a9adf3255a87c856
Yury Glushko <yury.glushko.web@gmail.com>
2022-08-20 21:58:31 +0300

    feat: added Message

---
+++ src/components/Message/Message.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import classNames from 'classnames'
+import css from './Message.module.scss'
+import {MessageType} from "../../interfaces/ChatTypes";
+import {USER} from "../../constants/constants";
+
+export default React.memo(({message, author}: MessageType) => {
+        return (
+            <div
+                className={classNames(
+                    css['message'],
+                    author == USER ? css['message__user'] : css['message__bot']
+                )}
+            >
+                {message}
+            </div>
+        )
+    }
+)
