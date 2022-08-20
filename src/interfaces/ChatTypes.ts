b4c7ee1d0e0dd7020aac417d7b99b44003b2199b
Yury Glushko <yury.glushko.web@gmail.com>
2022-08-20 21:56:11 +0300

    feat: added types in ChatTypes

--- src/interfaces/ChatTypes.ts
+++ src/interfaces/ChatTypes.ts
@@ -1,4 +1,8 @@
+import {BOT, USER} from "../constants";
+
+export type author = typeof USER | typeof BOT
+
 export interface MessageType {
-    author: string
+    author: author
     message: string
 }
