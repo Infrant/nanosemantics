f5ead414b7d57026b0eb4f97cfe148be8fd8a61d
Yury Glushko <yury.glushko.web@gmail.com>
2022-08-20 16:16:54 +0300

    feat: SendButton added prop isDisabled

--- src/components/SendButton/SendButton.tsx
+++ src/components/SendButton/SendButton.tsx
@@ -2,7 +2,11 @@ import React from "react";
 import LoadingButton from "@mui/lab/LoadingButton";
 import SendIcon from "@mui/icons-material/Send";
 
-export default () => {
+interface SendButtonProps {
+    isDisabled: boolean
+}
+
+export default ({isDisabled}: SendButtonProps) => {
     return (
             <LoadingButton
                 size="small"
@@ -11,6 +15,7 @@ export default () => {
                 // loading={loading}
                 loadingPosition="end"
                 variant="contained"
+                disabled={isDisabled}
                 style={{height: '38px', minWidth: "90px"}}
             >
                 Send
