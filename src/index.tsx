76a020dbfdb1e5d9b64335fa0c1efe81e6de47f5
Yury Glushko <yury.glushko.web@gmail.com>
2022-08-20 22:40:30 +0300

    feat: added Redux-Persist

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,17 @@
 import * as ReactDOM from "react-dom/client";
 import {Provider} from "react-redux";
+import {PersistGate} from 'redux-persist/integration/react'
 import {store} from "./store/store";
 import App from "./App";
+import {persistStore} from "redux-persist";
+
+let persistor = persistStore(store)
 
 const root = ReactDOM.createRoot(document.getElementById('root') as Element)
 root.render(
     <Provider store={store}>
-        <App/>
+        <PersistGate loading={null} persistor={persistor}>
+            <App/>
+        </PersistGate>
     </Provider>
 )
