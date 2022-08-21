625b0f32e43c37ca4138e43f056f372199c86236
Yury Glushko <yury.glushko.web@gmail.com>
2022-08-21 19:15:13 +0300

    feat: added types for api

--- src/interfaces/ChatTypes.ts
+++ src/interfaces/ChatTypes.ts
@@ -6,3 +6,50 @@ export interface MessageType {
     author: author
     message: string
 }
+
+export interface ChatState {
+    error: string
+    cuid: string
+    isLoading: boolean
+    isSendingMessage: boolean
+    gotGreetMessage: boolean
+    messages: MessageType[]
+}
+
+export type InitChatPayload = {
+    uuid: string
+    cuid?: string
+}
+
+export interface InitChatResponse  {
+    result?: {
+        cuid: string
+    }
+    error?: {
+        code: string
+        message: string
+    }
+}
+
+export interface RequestChatResponse  {
+    result?: {
+        cuid: string
+        text: {
+            value: string
+        }
+    }
+    error?: {
+        code: string
+        message: string
+    }
+}
+
+export type RequestChatPayload = {
+    cuid: string
+    text: string
+}
+
+export type EventChatPayload = {
+    cuid: string
+    euid?: string
+}
